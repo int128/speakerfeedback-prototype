22b5f179338c539d1aedbb32c7bf455b91348ee6
int128 <int128@gmail.com>
2014-04-01 00:43:37 +0900

    Add WebSocket example

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,25 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import play.api.libs.iteratee._
+import play.api.mvc.WebSocket
+import play.api.mvc.Controller
+import play.api.mvc.Action
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { implicit request =>
+    Ok(views.html.index(request))
+  }
+
+  def pollSocket(pollId: String) = WebSocket.using[String] { request =>
+    val (out, channel) = Concurrent.broadcast[String]
+
+    val in = Iteratee.foreach[String] {
+      message => channel.push(message)
+    }
+
+    (in, out)
   }
 
 }
